26541d2cef36b802ef1fd184424b72cbe271235b
Ashwin <ashwin.sagodira@epitech.eu>
2022-12-09 04:46:11 +0100

    [ADD] list of dictionnary to get the sense of decisional tree with the parameter of each command "Yes" or "No" redirecting directly to the right function after

--- src/ranking/tree.py
+++ src/ranking/tree.py
@@ -3,46 +3,69 @@
 class Decisional_tree:
     def __init__(self):
         self.step = 0
-        self.number_of_steps = 3
         self.score = 0
+        self.number_of_steps = 3
+        self.last_action = "Nothing"
+        self.list_of_commands = []
         self.commands = {
-            0: self.is_malaise,
-            1: self.is_symptome,
-            2: self.is_cardiac_arrest,
+            "MALAISE": self.is_malaise,
+            "SYMPTOME": self.is_symptome,
+            "CARDIAC_ARREST": self.is_cardiac_arrest,
+            "NONE": "Nothing"
         }
 
     def parse_conf(self):
         try:
             conf_file = open("commands.conf", "r")
-            self.commands.clear()
             self.reorder_from_conf(conf_file)
         except FileNotFoundError:
             return
 
-    def reorder_from_conf(self, file):
+    def create_dictionary(self, line): #put in dictionary and after that we put it in a list of dictionary
+        _dict = {}
+        match line[0]:
+            case "MALAISE":
+                _dict = {"Question": self.is_malaise, "Order": int(line[1]),  "YES": self.commands[line[3]], "NO": self.commands[line[5].strip()]}
+                self.list_of_commands.append(_dict)
+            case "CARDIAC_ARREST":
+                _dict = {"Question": self.is_cardiac_arrest, "Order": int(line[1]),  "YES": self.commands[line[3]], "NO": self.commands[line[5].strip()]}
+                self.list_of_commands.append(_dict)
+            case "SYMPTOME":
+                _dict = {"Question": self.is_symptome, "Order": int(line[1]),  "YES": self.commands[line[3]], "NO": self.commands[line[5].strip()]}
+                self.list_of_commands.append(_dict)
+
+    def reorder_from_conf(self, file): #create a list of dictionary
+        line = []
         for i in file:
-            match i.split(" ")[0]:
-                case "MALAISE":
-                    self.commands[int(i.split(" ")[1])] = self.is_malaise
-                case "CARDIAC_ARREST":
-                    self.commands[int(i.split(" ")[1])] = self.is_cardiac_arrest
-                case "SYMPTOME":
-                    self.commands[int(i.split(" ")[1])] = self.is_symptome
-                case "NB_COMMANDS":
-                    self.number_of_steps = int(i.split(" ")[1])
+            line = i.split(" ")
+            self.create_dictionary(line)
+                
+            
+    def ask_question(self): #question to ask from order of list and reponse yes or no will auomatically ask the right question
+        if (self.step == 0):
+            self.list_of_commands[self.step]["Question"]()
+            self.step += 1
+            return
+        else:
+            self.list_of_commands[self.step - 1][self.last_action]()
+            self.step += 1
+            return
 
-    def is_malaise(self, line_input):
-        if str(line_input) == "Oui":
+    def is_malaise(self):
+        print("is_malaise")
+        if self.last_action == "Oui":
             self.score += 10
 
 
-    def is_cardiac_arrest(self, line_input):
-        if line_input == "Oui":
+    def is_cardiac_arrest(self):
+        print("is_cardiac_arrest")
+        if self.last_action == "Oui":
             self.score += 10
 
 
-    def is_symptome(self, line_input):
-        if line_input == "Oui":
+    def is_symptome(self):
+        print("is_symptome")
+        if self.last_action == "Oui":
             self.score += 10
 
 
@@ -51,11 +74,12 @@ class Decisional_tree:
 
         while loop_status:
             line_input = input()
+            self.last_action = line_input
+            #                                      call ask_question
+            #                                      have to update self.last_action to "yes" or "NO"
 
             # to put in another function who will dispatch it :
-            self.is_cardiac_arrest(line_input)
-            self.is_symptome(line_input)
-            self.is_malaise(line_input)
+            self.ask_question()
 
             if str(line_input) == "QUIT":
                 loop_status = False
@@ -64,11 +88,9 @@ class Decisional_tree:
 def main():
     tree = Decisional_tree()
     tree.parse_conf()
-    
-    tree.get_line_loop()
-    
-    #for i in range (tree.number_of_steps):
-    #    tree.commands[i]()
+
+    #tree.ask_question()        #my function to test the call of function already in the order given in the conf file
+    #tree.get_line_loop()
     
 
 if (__name__ == "__main__"):
