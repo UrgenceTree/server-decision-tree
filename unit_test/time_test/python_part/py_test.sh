7135f2740b0fa11ddbac4e7fe67532433fcd095e
Reda-R <mohamed-reda.ramdani@epitech.eu>
2023-06-20 19:37:23 +0200

    [ADD] time test for the python part algorithm

---
+++ unit_test/time_test/python_part/py_test.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+
+# Création d'un fichier de test
+echo "yes" > data_answer.txt
+echo "yes" >> data_answer.txt
+echo "yes" >> data_answer.txt
+
+# Exécution du script à tester
+output=$(./tree.py -f data_answer.txt)
+
+# Vérification du résultat
+expected_output="Score final du patient : 110"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 1 réussi -> $output"
+else
+    echo "Py Test 1 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+# Nettoyage des fichiers temporaires
+rm -f data_answer.txt
+
+
+################################
+
+echo "yes" > data_answer.txt
+echo "yes" >> data_answer.txt
+echo "no" >> data_answer.txt
+
+output=$(./tree.py -f data_answer.txt)
+
+expected_output="Score final du patient : 110"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 2 réussi -> $output"
+else
+    echo "Py Test 2 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+rm -f data_answer.txt
+
+
+################################
+
+echo "yes" > data_answer.txt
+echo "no" >> data_answer.txt
+echo "no" >> data_answer.txt
+
+output=$(./tree.py -f data_answer.txt)
+
+expected_output="Score final du patient : 10"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 3 réussi -> $output"
+else
+    echo "Py Test 3 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+rm -f data_answer.txt
+
+
+################################
+
+echo "yes" > data_answer.txt
+echo "no" >> data_answer.txt
+echo "yes" >> data_answer.txt
+
+output=$(./tree.py -f data_answer.txt)
+
+expected_output="Score final du patient : 20"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 3 réussi -> $output"
+else
+    echo "Py Test 3 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+rm -f data_answer.txt
+
+################################
+
+echo "no" > data_answer.txt
+echo "no" >> data_answer.txt
+echo "no" >> data_answer.txt
+
+# Exécution du script à tester
+output=$(./tree.py -f data_answer.txt)
+
+# Vérification du résultat
+expected_output="Score final du patient : 0"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 4 réussi -> $output"
+else
+    echo "Py Test 4 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+# Nettoyage des fichiers temporaires
+rm -f data_answer.txt
+
+
+################################
+
+echo "no" > data_answer.txt
+echo "no" >> data_answer.txt
+echo "yes" >> data_answer.txt
+
+output=$(./tree.py -f data_answer.txt)
+
+expected_output="Score final du patient : 0"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 5 réussi -> $output"
+else
+    echo "Py Test 5 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+rm -f data_answer.txt
+
+
+################################
+
+echo "no" > data_answer.txt
+echo "yes" >> data_answer.txt
+echo "yes" >> data_answer.txt
+
+output=$(./tree.py -f data_answer.txt)
+
+expected_output="Score final du patient : 0"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 6 réussi -> $output"
+else
+    echo "Py Test 6 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+rm -f data_answer.txt
+
+
+################################
+
+echo "no" > data_answer.txt
+echo "yes" >> data_answer.txt
+echo "no" >> data_answer.txt
+
+output=$(./tree.py -f data_answer.txt)
+
+expected_output="Score final du patient : 0"
+if [[ "$output" == "$expected_output" ]]; then
+    echo "Py Test 7 réussi -> $output"
+else
+    echo "Py Test 7 échoué -> Le résultat obtenu ($output) ne correspond pas au résultat attendu ($expected_output)"
+fi
+
+rm -f data_answer.txt
---
+++ unit_test/time_test/python_part/tree.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+from questions import *
+import sys
+import configparser
+import argparse
+
+class Decisional_tree:
+    def __init__(self, filename):
+        self.file_path = filename
+        self.answer_data = self.get_file_contents()
+        self.qt = Questions()
+        # self.rq = request()
+        self.step = 0
+        self.score = 0
+        self.number_of_steps = 3
+        self.last_action = "First"
+        self.list_of_commands = []
+        self.action_order = []
+        self.serv_data_ip = ""
+        self.serv_data_port = 0
+
+
+    def get_file_contents(self):
+        tmp = []
+        with open(self.file_path , 'r') as file:
+            for line in file:
+                stock_value = line.strip()
+                tmp.append(stock_value)
+        return tmp
+        
+
+
+    def init_env(self):
+        config = configparser.ConfigParser()
+        config.read('./env/serv_data_info.env')
+        self.serv_data_ip = config['server_data_info']['SERV_DATA_IP']
+        self.serv_data_port = config['server_data_info']['SERV_DATA_PORT']
+
+    def handle_env(self):
+        self.init_env()
+        if ((int(self.serv_data_port) < 1025) or (int(self.serv_data_port) > 65535)):
+            print("ERROR: The PORT in the env must be between 1025 and 65535", file=sys.stderr)
+            exit(84)
+        if (self.serv_data_ip == None):
+            print("ERROR: Bad ip adress", file=sys.stderr)
+            exit(84)
+        #print("serv_data_ip =", self.serv_data_ip)
+        #print("serv_data_port =", self.serv_data_port)
+        self.name = ""
+        self.surname = ""
+
+    def parse_conf(self):
+        conf_file = open("commands.conf", "r")
+        self.reorder_from_conf(conf_file)
+
+    def reorder_from_conf(self, file): #create a list of dictionary
+        line = []
+        for i in file:
+            line = i.split(" ")
+            self.create_dictionary(line)
+
+    def create_dictionary(self, line): #put in dictionary and after that we put it in a list of dictionary
+        _dict = {}
+        
+        if line[0] == "NB_COMMANDS":
+            self.number_of_steps = int(line[1])
+            return
+        else:
+            _dict = {"Question": getattr(self.qt, line[0].capitalize()), "Order": int(line[1]), "Yes.": getattr(self.qt, line[3].capitalize()), "No.": getattr(self.qt, line[5].strip().capitalize())}
+            self.list_of_commands.append(_dict)
+            
+    def ask_question(self): #question to ask from order of list and reponse yes or no will auomatically ask the right question
+        if (self.last_action == "First"):
+            self.list_of_commands[self.step]["Question"](self)
+            self.step += 1
+            return
+        else:
+            self.list_of_commands[self.step - 1][self.last_action](self)
+            self.step += 1
+            return
+
+    def get_line_loop(self):
+        loop_status = True
+
+        #print("Qu'elle est votre prenom ?")
+        #self.name = input().strip()
+        #POST
+        # self.rq.create_patient()
+
+        while loop_status:
+            # Call ask_question
+            # Have to update self.last_action to "yes" or "NO"
+            self.ask_question()
+            #PUT
+            if self.last_action == "Nothing":
+                break
+            if self.last_action == "Yes." or self.last_action == "No.":
+                self.last_action = self.last_action
+            else:
+                self.step -= 1
+                self.score -= 10
+            if str(self.last_action) == "QUIT":
+                loop_status = False
+
+
+def main(args):
+
+    tree = Decisional_tree(args.filename)
+    ## try:
+    tree.parse_conf()
+    tree.get_line_loop()
+    print("Score final du patient :", tree.score)
+    # tree.rq.update_status()
+    # The function 'handle_env' get and set variables env to the class 'Decisional_tree'
+    # and handle errors from env variables set in env/server_data_info.env
+    #tree.handle_env()
+
+    #     print("\nThe Score:", tree.score)
+    # except KeyboardInterrupt:
+    #     print("ERROR: Keyboard Interrupt")
+    # except EOFError:
+    #     print("ERROR: End of File")
+    # except FileNotFoundError:
+    #     print("ERROR: File not found")
+    # except AttributeError:
+    #     print("ERROR: Function given doesn't exist")
+
+if (__name__ == "__main__"):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--filename", help="file with answer data", required=True, type=str)
+    args = parser.parse_args()
+    main(args)
