52d4f2013cc48b4e60a4360b2bf333bdb4377353
Ashwin <ashwin@appkorak.com>
2023-09-13 18:31:18 +0100

    [ADD] parser and algorithm together

---
+++ src/parser_algo.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type jsonContent struct {
+	FirstQuestion string
+	Questions     []string
+	YesScore      []float64
+	NoScore       []float64
+	Link          []string
+}
+
+func fillScore(questions []interface{}, elem *jsonContent) {
+	tmpYes := []float64{}
+	tmpNo := []float64{}
+
+	for i := 0; i < len(questions); i++ {
+		firstQuestion := questions[i].(map[string]interface{})
+		yesScore := firstQuestion["yes_score"].(float64)
+		noScore := firstQuestion["no_score"].(float64)
+		tmpYes = append(tmpYes, yesScore)
+		tmpNo = append(tmpNo, noScore)
+	}
+
+	elem.YesScore = tmpYes
+	elem.NoScore = tmpNo
+}
+
+func fillQuestions(questions []interface{}, elem *jsonContent) {
+	tmpQuestions := []string{}
+
+	for i := 0; i < len(questions); i++ {
+		currQuestion := questions[i].(map[string]interface{})
+		tmp := currQuestion["question"].(string)
+		tmpQuestions = append(tmpQuestions, tmp)
+	}
+
+	elem.Questions = tmpQuestions
+}
+
+func fillLinks(questions []interface{}, elem *jsonContent) {
+	tmpLinks := []string{}
+
+	for i := 0; i < len(questions); i++ {
+		currLink := questions[i].(map[string]interface{})
+		tmp := currLink["link"].(string)
+		tmpLinks = append(tmpLinks, tmp)
+	}
+
+	elem.Link = tmpLinks
+}
+
+func orderLists(firstQuestion string, elem *jsonContent) {
+	newQuestions := []string{}
+	newyesScore := []float64{}
+	newnoScore := []float64{}
+	newLink := []string{}
+	target := ""
+
+	for i := 0; i < len(elem.Questions); i++ {
+		curr := elem.Questions[i]
+		if curr == firstQuestion {
+			newQuestions = append(newQuestions, curr)
+			newyesScore = append(newyesScore, elem.YesScore[i])
+			newnoScore = append(newnoScore, elem.NoScore[i])
+			newLink = append(newLink, elem.Link[i])
+			target = elem.Link[i]
+			break
+		}
+	}
+
+	for i := 0; i < len(elem.Questions); i++ {
+		curr := elem.Questions[i]
+		if curr == target {
+			newQuestions = append(newQuestions, curr)
+			newyesScore = append(newyesScore, elem.YesScore[i])
+			newnoScore = append(newnoScore, elem.NoScore[i])
+			newLink = append(newLink, elem.Link[i])
+			target = elem.Link[i]
+			i = -1
+		}
+	}
+
+	elem.Questions = newQuestions
+	elem.YesScore = newyesScore
+	elem.NoScore = newnoScore
+	elem.Link = newLink	
+}
+
+func main() {
+	jsonFile, err := os.Open("adc.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
+
+	questions := result["questions"].([]interface{})
+	elem := jsonContent{}
+
+	elem.FirstQuestion = result["starting_question"].(string)
+	fillScore(questions, &elem)
+	fillQuestions(questions, &elem)
+	fillLinks(questions, &elem)
+	
+	orderLists(elem.FirstQuestion, &elem)
+
+	var responses []string
+
+	i := 0
+	for i < len(elem.Questions) {
+		fmt.Printf("Question %d: %s\n", i, elem.Questions[i])
+		var response string
+		fmt.Scanln(&response)
+		responses = append(responses, response)
+
+		if elem.Link[i] == "null" {
+			fmt.Println("\nEnd of questions.\n")
+			break
+		}
+		i++
+	}
+
+	for i, response := range responses {
+		fmt.Printf("Response %d: %s\n", i, response)
+	}
+}
